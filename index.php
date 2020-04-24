1d6341a09ede4372e64482443c295e1dd89f2447
Tiago Martins <vargas.tiago@hotmail.com>
2020-04-24 14:02:35 -0300

    Added cart styles

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 
-<h1><?php the_title(); ?></h1>
-<h1><?php the_content(); ?></h1>
+<h1 class="title"><?php the_title(); ?></h1>
+<main class="container container-index"><?php the_content(); ?></main>
 
 
 <?php } }?>
